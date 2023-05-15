4e282d2f45481be3d5f67aab38157e554e6fb777
Anwar <anwarhussaini160@gmail.com>
2023-05-15 23:50:27 +0430

    Prepare environment for collaboration

--- javascripts/script.js
+++ javascripts/script.js
@@ -1,20 +1,20 @@
 // Variables
 const menuIcon = document.querySelector('.menu-icon');
 const closeBtn = document.querySelector('.close-btn');
-const navBar = document.querySelector('.mobile-nav');
+// const navBar = document.querySelector('.mobile-nav');
 const menuItems = document.querySelectorAll('.mobile-nav .menu-item');
 
 // Actions
 menuIcon.addEventListener('click', () => {
-  navBar.classList.add('is-active');
+  // Write your code here
 });
 
 closeBtn.addEventListener('click', () => {
-  navBar.classList.remove('is-active');
+  // Write your code here
 });
 
 for (let i = 0; i < menuItems.length; i += 1) {
   menuItems[i].addEventListener('click', () => {
-    navBar.classList.remove('is-active');
+    // Write your code here
   });
 }
