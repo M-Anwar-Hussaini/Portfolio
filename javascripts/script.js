ce755f54d26e8fff4f7a8c43e0c24e24150da04f
Anwar <anwarhussaini160@gmail.com>
2023-05-15 23:43:15 +0430

    Add mobile menu to the portfolio

---
+++ javascripts/script.js
@@ -0,0 +1,20 @@
+// Variables
+const menuIcon = document.querySelector('.menu-icon');
+const closeBtn = document.querySelector('.close-btn');
+const navBar = document.querySelector('.mobile-nav');
+const menuItems = document.querySelectorAll('.mobile-nav .menu-item');
+
+// Actions
+menuIcon.addEventListener('click', () => {
+  navBar.classList.add('is-active');
+});
+
+closeBtn.addEventListener('click', () => {
+  navBar.classList.remove('is-active');
+});
+
+for (let i = 0; i < menuItems.length; i += 1) {
+  menuItems[i].addEventListener('click', () => {
+    navBar.classList.remove('is-active');
+  });
+}
