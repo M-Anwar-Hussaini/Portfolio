bbe5875820cb65b06f11e9e33faf7f95baf90e51
Anwar <anwarhussaini160@gmail.com>
2023-05-17 23:34:57 +0430

    Fix linters errors

--- javascripts/script.js
+++ javascripts/script.js
@@ -22,23 +22,3 @@ for (let i = 0; i < menuItems.length; i += 1) {
 /* *******************
 * FOR POP UP WINDOWS *
 ******************* */
-const projectBtns = document.querySelectorAll('btn--project');
-const allProjects = [
-  {
-    name: 'First Project',
-    description: 'lorem',
-    image: 'path/to/featured-image-1.jpg',
-    technologies: 'Technologies used in Project 1',
-    liveVersion: 'https://www.example.com/project1',
-    sourceCode: 'https://www.github.com/project1',
-  },
-  {
-    name: 'Project 2',
-    description: 'Description of Project 2',
-    image: 'path/to/featured-image-2.jpg',
-    technologies: 'Technologies used in Project 2',
-    liveVersion: 'https://www.example.com/project2',
-    sourceCode: 'https://www.github.com/project2',
-  },
-  // Add more projects as needed
-];
